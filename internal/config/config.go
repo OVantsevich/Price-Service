ed402c3b068e8430609955199324605ca1d65bad
olegvantsevich <OVantsevich@gmail.com>
2023-03-07 08:51:47 +0300

    07.03.2023 version 1.12

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,12 +9,12 @@ import (
 
 // Config config
 type Config struct {
-	RedisHost     string `env:"REDIS_HOST,notEmpty" envDefault:"localhost"`
-	RedisPort     string `env:"REDIS_PORT,notEmpty" envDefault:"6379"`
-	RedisPassword string `env:"REDIS_PASSWORD,notEmpty" envDefault:"redis"`
-	StreamName    string `env:"STREAM_NAME,notEmpty" envDefault:"prices"`
-	//GroupName         string `env:"GROUP_NAME,notEmpty" envDefault:"priceGroup"`
+	RedisHost         string `env:"REDIS_HOST,notEmpty" envDefault:"localhost"`
+	RedisPort         string `env:"REDIS_PORT,notEmpty" envDefault:"6379"`
+	RedisPassword     string `env:"REDIS_PASSWORD,notEmpty" envDefault:"redis"`
+	StreamName        string `env:"STREAM_NAME,notEmpty" envDefault:"prices"`
 	Port              string `env:"PORT,notEmpty" envDefault:"4000"`
+	Host              string `env:"HOST,notEmpty" envDefault:"localhost"`
 	PriceProviderPort string `env:"PRICE_PROVIDER_PORT,notEmpty" envDefault:"3000"`
 	PriceProviderHost string `env:"PRICE_PROVIDER_HOST,notEmpty" envDefault:"localhost"`
 }
